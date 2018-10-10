f5a289a88ccd5acaa39a4b183feb556ad539da48
Joe Vusich <jvusich@amazon.com>
2018-10-10 22:41:40 +0000

    Add UI page

--- web.go
+++ web.go
@@ -3,17 +3,20 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
 )
 
 func main() {
+	http.HandleFunc("/", UI)
 	http.HandleFunc("/add", Add)
 	http.HandleFunc("/subtract", Subtract)
 	http.HandleFunc("/multiply", Multiply)
 	http.HandleFunc("/divide", Divide)
 	fmt.Println("listening...")
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe(":8024", nil)
 	if err != nil {
 		panic(err)
 	}
@@ -25,7 +28,7 @@ func Add(res http.ResponseWriter, req *http.Request) {
 		RespondError(res, err.Error())
 		return
 	}
-	result := a + b;
+	result := a + b
 	RespondResult(res, result)
 }
 
@@ -35,7 +38,7 @@ func Subtract(res http.ResponseWriter, req *http.Request) {
 		RespondError(res, err.Error())
 		return
 	}
-	result := a - b;
+	result := a - b
 	RespondResult(res, result)
 }
 
@@ -45,7 +48,7 @@ func Multiply(res http.ResponseWriter, req *http.Request) {
 		RespondError(res, err.Error())
 		return
 	}
-	result := a * b;
+	result := a * b
 	RespondResult(res, result)
 }
 
@@ -55,11 +58,15 @@ func Divide(res http.ResponseWriter, req *http.Request) {
 		RespondError(res, err.Error())
 		return
 	}
-	result := a / b;
+	result := a / b
 	RespondResult(res, result)
 }
 
-func ParseArgs (req *http.Request) (float64, float64, error) {
+func UI(res http.ResponseWriter, req *http.Request) {
+	RespondHTML(res, "index.html")
+}
+
+func ParseArgs(req *http.Request) (float64, float64, error) {
 	a, err := strconv.ParseFloat(req.FormValue("a"), 64)
 	if err != nil {
 		return 0, 0, errors.New("bad first argument: " + req.FormValue("a"))
@@ -71,17 +78,29 @@ func ParseArgs (req *http.Request) (float64, float64, error) {
 	return a, b, nil
 }
 
-func RespondError (res http.ResponseWriter, message string) {
+func RespondError(res http.ResponseWriter, message string) {
 	res.Header().Set("Access-Control-Allow-Origin", "*")
 	res.Header().Set("Content-Type", "text/plain")
 	res.WriteHeader(http.StatusBadRequest)
 	fmt.Fprintln(res, message)
 }
 
-func RespondResult (res http.ResponseWriter, result float64) {
+func RespondResult(res http.ResponseWriter, result float64) {
 	res.Header().Set("Access-Control-Allow-Origin", "*")
 	res.Header().Set("Content-Type", "text/plain")
 	res.WriteHeader(http.StatusOK)
 	fmt.Fprintln(res, result)
 }
 
+func RespondHTML(res http.ResponseWriter, file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		RespondError(res, err.Error())
+		return
+	}
+	defer f.Close()
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	res.Header().Set("Content-Type", "text/html")
+	res.WriteHeader(http.StatusOK)
+	io.Copy(res, f)
+}
